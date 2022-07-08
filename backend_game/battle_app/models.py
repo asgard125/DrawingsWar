a2249ecce17bbb7de665917084592690fcbdc6d1
asgard125 <asgardkarpov@yandex.ru>
2022-07-09 05:29:57 +1000

    engine total remake, added queue of groups because it worked only for one group

--- backend_game/battle_app/models.py
+++ backend_game/battle_app/models.py
@@ -1,16 +1,19 @@
 from django.db import models
+from game_app.models import User
 import random
 # Create your models here.
 
 
 class BattleSession(models.Model):
     room_code = models.CharField(max_length=100)
+    started = models.BooleanField(default=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     players_count = models.IntegerField(default=0)
 
     @staticmethod
-    def get_available_room():
+    def get_available_room(user):
         rooms = BattleSession.objects.all().order_by('-room_code')
-        available_rooms = [i for i in rooms if i.players_count < 2]
+        available_rooms = [i for i in rooms if (i.players_count < 2 and i.creator != user)]
         if len(available_rooms) == 0:
             if len(rooms) > 0:
                 room_code = int(rooms[0].room_code) + 1
